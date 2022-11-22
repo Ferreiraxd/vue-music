639cdcf1c1dbe175527430e883c9b712f7184279
Ferreiraxd <miguelandres.ferreirasandoval@gmail.com>
2022-11-22 00:27:33 -0300

    🚧 putting new content into detail view

--- src/views/TrackDetail.vue
+++ src/views/TrackDetail.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="container">
     <div class="columns">
-      <div class="column is-3 has-text-centered">
+      <div class="column is-5 has-text-centered">
         <figure class="media-left">
           <p class="image">
             <img :src="track.album.images[0].url" alt="" />
           </p>
-          <p>
-            <a class="button is-primary is-large">
-              <span class="icon" @click="selectTrack"></span>
-            </a>
-          </p>
         </figure>
       </div>
-      <div class="column is-8">
+      <div class="column is-7">
         <div class="panel">
           <div class="panel-heading">
             <h1 class="title">
               {{ track.name }}
             </h1>
+            <img
+                  class="header-play-button"
+                  @click="selectTrack"
+                  src="@/assets/Images/music/icons8-play-button-circled-100.png"
+                />
           </div>
           <div class="panel-block">
-            <article class="media"></article>
+            <article class="media">
+              <p>Album : {{ track.album.name }}</p>
+              <p>Fecha de lanzamiento: {{track.album.release_date}} </p>
+              <p>Artistas: {{this.artistsNames}}</p>
+              <div class="popularity-box">
+                <p>Popularidad:  </p>
+                <star-rating
+                  :rating="this.ratingConvertion"
+                  :read-only="true"
+                  :star-points=8
+                  :fixed-points=1
+                  :star-size=20
+                >
+                </star-rating>
+              </div>
+            </article>
           </div>
         </div>
       </div>
@@ -31,11 +46,15 @@
 
 <script>
 import { mapState, mapActions } from "vuex";
+import StarRating from 'vue-star-rating'
 import trackMixin from "@/mixins/track";
 
 export default {
   name: "TrackDetail",
   mixins: [trackMixin],
+  components: {
+    StarRating
+  },
   data() {
     return {
       track: {},
@@ -43,6 +62,12 @@ export default {
   },
   computed: {
     ...mapState(["tracks"]),
+    artistsNames(){
+      return this.track.album.artists.map(artist => artist.name).join(" - ");
+    },
+    ratingConvertion(){
+      return (this.track.popularity * 5) / 100;
+    }
   },
   async created() {
     const id = this.$route.params.id;
@@ -55,10 +80,47 @@ export default {
 </script>
 
 <style scoped>
+* {
+  color: aliceblue;
+}
+
 .container {
   display: flex;
   height: 87vh;
   align-items: center;
   justify-content: center;
+}
+
+.title,
+.panel-heading {
+  background-color: #181624;
+}
+
+.media {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+
+.popularity-box {
+  display: flex;
+  gap: 5px;
+}
+
+.header-play-button {
+  max-height: 36px;
+}
+
+.panel-heading {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.panel-heading > h1 {
+  margin-bottom: unset;
+}
+
+.panel-block {
+  font-size: x-large;
 }
 </style>
